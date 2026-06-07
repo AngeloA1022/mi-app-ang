1b6ce3705103c6af6205122a0cd547c57bbfdb77
AngeloA1022 <angelo.armijo@inacapmail.cl>
2026-06-07 19:11:57 -0400

    cambios

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,8 +17,10 @@ export default function Navbar() {
         <li>
           <Link to="/contacto">Contacto</Link>
         </li>
-
-
+        <li>
+          <Link to="/personajes">Personajes</Link>
+        </li>
+        
         <li><Link to="/Registro">Registro</Link></li>
         <li><Link to="/login">Login</Link></li>
       </ul>
